3d9f7a557931292fdea237336046eda116eaca16
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2023-11-06 22:14:56 -0300

    update js

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numSecret=29;
+let num = prompt("Escolha um número entre 1 e 30");
+
+if(numSecret==num)
+    console.log("Parabéns! Você Acertou (29)!");
+else
+    console.log("Você Errou!");
